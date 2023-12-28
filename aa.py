6200e1c5207a5ac13e0c3652f8412171a4723289
Your Name <you@example.com>
2023-12-28 21:20:00 +0330

    the text was added

---
+++ aa.py
@@ -0,0 +1,29 @@
+
+
+def av(x,y):
+   a = (x+y) / 2
+   if a < x:
+      print("x is bigger than average")
+   else:
+      print("x is not bigger than average")
+
+   if a < y:
+      print("y is bigger than average")
+   else:
+      print("y is not bigger than average")
+
+av(int(input("entet your number: ")),int(input("entet your number: ")))
+
+x = 20
+y = 13
+av = (x+y) / 2
+
+if x > av:
+   print("x is bigger than average")
+else:
+   print("x is not bigger than average")
+
+if y > av:
+   print("y is bigger than average")
+else:
+   print("y is not bigger than average")
