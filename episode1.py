29d5bd4b0205ff27d931d753191be51e43aed29c
Your Name <you@example.com>
2024-02-02 09:55:51 +0330

    the text was added

--- episode1.py
+++ episode1.py
@@ -1,16 +1,14 @@
-# list = open("employees.txt" , "r+")
-# list.write("hello world!\n")
-# list.write("hello world!\n")
-# list.write("hello world!\n")
-# list.write("hello world!\n")
-# print(list.read())
-# for item in list.readlines():
-# #     print(item.strip("\n"))
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
-# # list.close()
 
-# with open("employees.txt") as list:
-#     for item in list.readlines():
-#         print(item.strip("\n"))
-list = open("employees.txt" , "w")
-list.write("homayoun")
+
+msg = MIMEMultipart()
+MIMEMultipart["from"] = "saeednikbakht928@gmail.com" 
+MIMEMultipart["to"] = 
+MIMEMultipart["subject"] = 
+msg.attach(MIMEText())
+with smtplib.SMTP(host="smtp.google.com" port=567) as server:
+
+
---
+++ oop1.py
@@ -0,0 +1,18 @@
+
+class Person:
+    def __init__(self,firstName,lastName,phoneNumber,age):
+        self.firstName = firstName
+        self.lastName = lastName 
+        self.phoneNumber = phoneNumber
+        self.age = age
+        
+
+Ali = Person("Ali","Mahmoodabadi","09122222222",15)
+Amirali = Person("Amirali","poorkhosravani","09123334444",15)
+Amirmohamad = Person("Amirmohamad","Abisinia","09121111111",15)
+Saeed = Person("Saeed","Nickbakht","09123450295",17)
+
+class Course:
+    student = [Person("saeed","nikbakht", "09385629204", "18"), Person("amir", "ali", "094r2834", "16"), Person("omid", "sina", "202e399", "13")]
+    teacher = [Person("gpt", "4", "9459344923", "144")]
+    time = Person()
